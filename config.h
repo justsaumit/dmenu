f7374c955f21fed6eb4e57d7541097547fd7a196
Saumit Dinesan <justsaumit@protonmail.com>
2022-10-26 15:56:04 +0530

    Personalized changes

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font:size=13",
+        "JoyPixels:pixelsize=15:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -16,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 28;
 static unsigned int min_lineheight = 8;
 
 /*
